d4eccbd9a505360576cb9d94f83716967beba4ce
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-20 14:07:29 +0900

    start

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :affiliation, presence: true
+         validates :post, presence: true
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'articles/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root to: "articles#index"
+end
