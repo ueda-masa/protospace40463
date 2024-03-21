c00a94044590c4b1c67e7c78b4efdce7dc8168c1
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-21 18:59:32 +0900

    3211

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,23 @@
+class PrototypesController < ApplicationController
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototypes = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)  
+    if @prototype.save
+      redirect_to '/'     # データが保存されたらルートパスにリダイレクト
+    else
+      render :new   # データが保存されなかったら新規投稿ページを表示
+    end  
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root to: "articles#index"
+   resources :prototypes, only: [:index, :new, :create]
 end
