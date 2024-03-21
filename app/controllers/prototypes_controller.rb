97e6cf373543bf57b7cf0df397f44dcfc00e8e28
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-21 21:26:11 +0900

    1

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,30 @@
+class PrototypesController < ApplicationController
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def edit
+  end
+
+  def new
+    @prototype = Prototype.new 
+  end
+  
+  def create
+    @prototype = Prototype.new(prototype_params)  
+    if @prototype.save
+      redirect_to '/'     # データが保存されたらルートパスにリダイレクト
+    else
+      render :new   # データが保存されなかったら新規投稿ページを表示
+    end  
+  end
+
+  def show
+    @prototype = Prototype.new(params[:id])
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+end
