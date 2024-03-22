bd5fc9a765896ec412672b95d6be8e2949282727
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-22 21:08:08 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    root to: "prototypes#index"
 
-   resources :prototypes, only: [:index, :new, :create, :edit, :show, :update, :destroy]
+   resources :prototypes #, only: [:index, :new, :create, :edit, :show, :update, :destroy]
 
 end
