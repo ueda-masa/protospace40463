d6cda1beb9248b9f2e96094726bfe18eefabd069
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-24 20:01:06 +0900

    first  commit..

--- db/migrate/20240324013045_add_details_to_comments.rb
+++ db/migrate/20240324013045_add_details_to_comments.rb
@@ -1,7 +1,7 @@
 class AddDetailsToComments < ActiveRecord::Migration[7.0]
   def change
-    add_column :comments, :text, :string
+    #add_column :comments, :text, :string
     #add_reference :comments, :user, null: false, foreign_key: true
-    add_reference :comments, :prototype, null: false, foreign_key: true
+    #add_reference :comments, :prototype, null: false, foreign_key: true
   end
 end
