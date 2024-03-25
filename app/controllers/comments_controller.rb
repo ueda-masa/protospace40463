311e2f4ed7e68f8a56523b94adc8c613a3906366
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-25 14:38:40 +0900

    forth commit2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to @comment.prototype
+      redirect_to prototype_comments_path(@prototype)
     else
-      @prototype = Prototype.find(params[:prototype_id])
       @comments = @prototype.comments.includes(:user)
-      @comment = @prototype.comments.build # <= コメントが保存できなかったときに新しいインスタンスを作成して再度表示する
-      render "prototypes/show"
+      render :prototypes.show :unprocessable_entity
     end
   end
 
@@ -17,5 +15,4 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
-
 
