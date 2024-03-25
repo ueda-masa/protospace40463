2cc72ed7403af88f5611ee676b41086c832f9404
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-25 15:10:45 +0900

    five commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,12 @@
 class CommentsController < ApplicationController
 
-  def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to prototype_comments_path(@prototype)
-    else
-      @comments = @prototype.comments.includes(:user)
-      render 'prototypes/show', status: :unprocessable_entity
-    end
-  end
-
-  private
-  def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
-  end
+def create
+  comment = Comment.create(comment_params)
+  redirect_to "/prototypes/#{comment.prototype.id}"  
 end
 
+private
+def comment_params
+  params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+end
+end
