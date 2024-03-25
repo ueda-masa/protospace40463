8722e2fa8fe1e264f6c6dce67df304c325ea7a8b
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-25 14:59:09 +0900

    forth commit4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_comments_path(@prototype)
     else
       @comments = @prototype.comments.includes(:user)
-      render :prototypes.show :unprocessable_entity
+      render 'prototypes/show', status: :unprocessable_entity
     end
   end
 
